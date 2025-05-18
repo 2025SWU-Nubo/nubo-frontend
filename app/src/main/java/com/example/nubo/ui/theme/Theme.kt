d3042bbffbc943b61d30c5d7d04903a140e57287
LeeYeso <leeyeso0315@gmail.com>
2025-05-18 20:50:18 +0900

    feat: App Color.kt 파일 추가

--- app/src/main/java/com/example/nubo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nubo/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.example.nubo.ui.theme
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -10,3 +11,15 @@ fun NuboAppTheme(content: @Composable () -> Unit) {
         content = content
     )
 }
+
+// LightColor
+private val LightColorScheme = lightColorScheme(
+    primary = PurpleMain500,
+    onPrimary = OnDarkText,
+    secondary = GreyMain300,
+    background = WhiteBg,
+    onBackground = DefaultText,
+    surface = WhiteBg,
+    onSurface = DefaultText,
+    error = PinkError
+)
