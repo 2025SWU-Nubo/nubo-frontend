0fdcb32951fdf234888be7586f6e4c75ec96f06e
Kosieun <sieun1156@naver.com>
2025-05-18 18:09:50 +0900

    feat : add font family

---
+++ app/src/main/java/com/example/nubo/ui/theme/AppFonts.kt
@@ -0,0 +1,15 @@
+package com.example.nubo.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.nubo.R
+
+object AppFonts {
+    val Pretendard = FontFamily(
+        Font(R.font.pretendard_regular, FontWeight.Normal),
+        Font(R.font.pretendard_medium, FontWeight.Medium),
+        Font(R.font.pretendard_semi_bold, FontWeight.SemiBold),
+        Font(R.font.pretendard_bold, FontWeight.Bold),
+    )
+}
