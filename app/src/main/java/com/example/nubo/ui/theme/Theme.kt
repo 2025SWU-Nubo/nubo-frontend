90cd9fba31e030c92bee4e4f984626ac825c0c29
Kosieun <sieun1156@naver.com>
2025-05-19 12:53:29 +0900

    feat : first main page ui and add color.kt

--- app/src/main/java/com/example/nubo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/nubo/ui/theme/Theme.kt
@@ -1,11 +1,18 @@
 package com.example.nubo.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 
 @Composable
-fun NuboAppTheme(content: @Composable () -> Unit) {
+fun NuboAppTheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (useDarkTheme) DarkColorScheme else LightColorScheme
+
     MaterialTheme(
+        colorScheme = colors,
         typography = AppTypography,
         content = content
     )
