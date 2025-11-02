84c260ad8c36194bc35df3b9ef164b6972199ec0
LeeYeso <leeyeso0315@gmail.com>
2025-11-02 14:37:04 +0900

    fix: 임포트 추가하여 오류 해결

--- app/src/main/java/com/example/nubo/ui/screen/myBoard/SectionDetailScreen.kt
+++ app/src/main/java/com/example/nubo/ui/screen/myBoard/SectionDetailScreen.kt
@@ -11,12 +11,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
